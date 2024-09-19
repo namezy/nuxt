45cde4c7b6324830dd53efdc9d9b09ee6f003475
namezy <243135193@qq.com>
2024-09-19 08:12:16 +0800

    first commit

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>hello</h2>
+    <button class="text-purple-500">按钮</button>
+    <NuxtRouteAnnouncer />
+  </div>
+</template>
+
+
+
+
+<script setup lang="ts">
+</script>
