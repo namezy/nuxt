84c4a84965331d01b3c283bd4a4cbded51a99c73
namezy <243135193@qq.com>
2024-09-22 22:45:59 +0800

    解决闪烁问题

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ import { NaiveUiResolver } from "unplugin-vue-components/resolvers"
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["nuxtjs-naive-ui", "nuxt-windicss", "@nuxt/eslint"],//@bg-dev/nuxt-naiveui
+  modules: ["nuxtjs-naive-ui", "nuxt-windicss", "@nuxt/eslint"], //@bg-dev/nuxt-naiveui
   css: ["@/assets/styles/global.css"],
   vite: {
     plugins: [
